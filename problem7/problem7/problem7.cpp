98bb7d92d1c7dfb3288795a0be61d66a84255e88
Albert Stark <albertecstark@gmail.com>
2021-12-17 15:58:54 +0000

    problem 7
    not proud of this one, ended up just printing out all the prime numbers under a certain limit, and fiddled with it until there were 10001 lines in the output.txt file

---
+++ problem7/problem7/problem7.cpp
@@ -0,0 +1,31 @@
+﻿/*
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+ */
+#include <iostream>
+
+bool isPrime(int p)
+{
+	if(p==0||p==1) return false;
+
+	for(int i = 2; i < static_cast<int>(sqrt(p)) + 1; ++i)
+	{
+		if(p % i == 0) return false;
+	}
+
+	return true;
+}
+
+int main()
+{
+	int k = 0;
+	while(k<105000)
+	{
+		if(isPrime(k) == true)
+		{
+			std::cout << k << "\n";
+		}
+		++k;
+	}
+}
