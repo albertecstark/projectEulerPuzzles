bbc6bdc116f6dae0a4a6ac53899a3c7b6cb2c3e7
albert <albertecstark@gmail.com>
2022-01-14 20:05:20 +0000

    Problem256 setup

--- problem30/main.py
+++ problem30/main.py
@@ -3,13 +3,23 @@
 # 1634 = 1^4 + 6^4 + 3^4 + 4^4
 # 8208 = 8^4 + 2^4 + 0^4 + 8^4
 # 9474 = 9^4 + 4^4 + 7^4 + 4^4
-# As 1 = 14 is not a sum it is not included.
+# As 1 = 1^4 is not a sum it is not included.
 #
 # The sum of these numbers is 1634 + 8208 + 9474 = 19316.
 #
 # Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# start by getting a number, parsing it into its digits as a list, exponentiating those, if the sum is equal to the original number, append these to a list of trues
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# start by getting a number, parsing it into its digits as a list, exponentiating those,
+#       if the sum is equal to the original number,
+#               append these to a list of trues
+#
+# This method isn't at all rigorous, but if I test to a high enough value, and new values stop appearing,
+#   then I can assume that this is correct
+# I have tested up to 10,000,000 which returned the same as 1,000,000 , which was the same as 200,000
+#   hopefully it's not the case that there is in fact a next term, and I need to go further
+# TODO: attach images of the proof that I think of that this is in fact correct
 import math
 
 trues = []
@@ -21,20 +31,25 @@ def digits(x):
 
 
 def main(limit):
-    for i in range(limit):
+    for i in range(2, limit):
+        # remember, 1 doesn't count as a valid solution
         s = 0
         dgt = digits(i)
         for j in range(len(dgt)):
-            s += math.pow(dgt[j], 5)
+            val = math.pow(int(dgt[j]), 4)
+            s += val
             if s == i:
                 trues.append(s)
-            #     print(f' appended {i}')
-            # else:
-            #     print(f'-- {i}')
+                print(f' appended {i}')
+            else:
+                print(f'-- {i}')
 
-    return trues
-
-
-if '__name__' == '__main__':
-    main(10000)
+    print(trues)
     print(sum(trues))
+    sum_dgt = sum(trues)
+    return sum_dgt
+
+
+user_in = input('please type the number that you want to test up to (integer)')
+
+main(int(user_in))
